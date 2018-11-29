82858dd8f5eaa7d2b431ab2a17565af35242af5a
Артем Горбачев <meltaus@yandex.ru>
2018-11-29 22:51:31 +0700

    Создание массива и заполнение его случайными целыми числами

--- Lesson2/src/main/java/ru/gorbachev/geekbrains/algoritm/lesson2/Lesson2.java
+++ Lesson2/src/main/java/ru/gorbachev/geekbrains/algoritm/lesson2/Lesson2.java
@@ -1,8 +1,12 @@
 package ru.gorbachev.geekbrains.algoritm.lesson2;
 
+import ru.gorbachev.geekbrains.algoritm.lesson2.object.BigArray;
+
 public class Lesson2 {
 
-    public static void main(String[] args) {
+    private static BigArray bigArray;
 
+    public static void main(String[] args) {
+        bigArray = new BigArray(-5);
     }
 }
--- Lesson2/src/main/java/ru/gorbachev/geekbrains/algoritm/lesson2/object/BigArray.java
+++ Lesson2/src/main/java/ru/gorbachev/geekbrains/algoritm/lesson2/object/BigArray.java
@@ -1,4 +1,33 @@
 package ru.gorbachev.geekbrains.algoritm.lesson2.object;
 
 public class BigArray {
+
+    private int size_array;
+    private int[] array;        //Массив целых чисел
+
+    public BigArray(int size_array) {
+        if (size_array <= 0) {
+            size_array = 1000000;
+        }
+        this.size_array = size_array;
+        createArray();
+    }
+
+    public BigArray() {
+        this.size_array = 1000000;
+        createArray();
+    }
+
+    private void createArray() {
+        array = new int[size_array];
+        //Заполняем массив случайными числами от 0 до size_array/2
+        int half_size_array = size_array/2;
+        for (int i = 0; i < size_array; i ++) {
+            array[i] = (int) ( Math.random() * half_size_array );
+        }
+    }
+
+    public int[] getArray() {
+        return array;
+    }
 }
